015e0ae8c399ab149b0efdfe74c08dfb7a70ca23
vuvannghiawork <vuvannghia.work@gmail.com>
2026-03-01 09:48:52 +0700

    Add initial project setup with FastAPI and configuration (#2)
    - Created pyproject.toml for project metadata and dependencies.
    - Added main.py to set up FastAPI application with a root endpoint.
    - Introduced settings.py for environment variable management using environs and loguru.
    - Created index_router.py for future routing capabilities.

---
+++ src/main.py
@@ -0,0 +1,17 @@
+import settings
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {
+        # "message": "Hello World",
+        "docs": "/docs"
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host=settings.IP, port=settings.PORT, reload=True)
